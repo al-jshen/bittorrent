af612b6616878cd0a070d6c14b35e1a52e6834a7
Jeff Shen <jshen2014@hotmail.com>
2020-10-29 02:14:05 -0400

    test string and integer decoding together

--- src/bencoding/decode.rs
+++ src/bencoding/decode.rs
@@ -8,35 +8,45 @@ pub fn decode_file(filepath: &str) {
 }
 
 /// Decodes a Bencoded array of bytes
-pub fn decode(bytes: &[u8]) -> Vec<u8> {
+pub fn decode(bytes: &[u8]) -> Vec<String> {
     let mut position = 0;
-    let mut decoded: Vec<u8> = Vec::new();
-    println!("bytes.len={}", bytes.len());
+    let mut decoded: Vec<String> = Vec::new();
+    // println!("bytes.len={}", bytes.len());
     while position < bytes.len() {
-        println!("position={} outerdecoded={:?}", position, decoded);
+        // println!(
+        //     "position={} outerdecoded={:?}, bytes={:?}",
+        //     position, decoded, bytes
+        // );
         // TODO: dont pass the entire array of bytes each time
         let (partial, advanced) = decode_next(bytes, position);
+        // println!("partial {} advanced {}", partial, advanced);
         // TODO: convert partial to proper type (eg. using bytes_to_integer)
-        decoded.extend(partial);
+        decoded.push(partial);
+        // println!("pushed");
         position += advanced;
+        // println!("advanced");
+        // println!("position {}", position);
     }
     decoded
 }
 
-fn decode_next(bytes: &[u8], position: usize) -> (Vec<u8>, usize) {
-    match bytes[position] {
-        100 => decode_dictionary(bytes, position), // "d" (100) marks beginning of dictionary
-        105 => decode_integer(bytes),              // "i" (105) marks beginning of integer
-        108 => decode_list(bytes, position),       // "l" (108) marks beginning of list
+fn decode_next(bytes: &[u8], position: usize) -> (String, usize) {
+    let (decoded_bytes, advanced) = match bytes[position] {
+        // 100 => decode_dictionary(bytes, position), // "d" (100) marks beginning of dictionary
+        105 => decode_integer(bytes, position), // "i" (105) marks beginning of integer
+        // 108 => decode_list(bytes, position),       // "l" (108) marks beginning of list
         _ => decode_string(bytes, position),
-    }
+    };
+    // println!("decoded bytes={:?}", decoded_bytes);
+    (bytes_to_string(&decoded_bytes), advanced)
 }
 
-fn decode_integer(bytes: &[u8]) -> (Vec<u8>, usize) {
+fn decode_integer(bytes: &[u8], position: usize) -> (Vec<u8>, usize) {
     // TODO: make encodings with leading 0s fail (i03e is invalid)
     let mut int: Vec<u8> = Vec::new();
-    let mut bytes_iter = bytes.iter();
-    let mut advanced = 1;
+    let mut advanced = position;
+    let mut bytes_iter = bytes[advanced..].iter();
+    // println!("initial advanced={}", advanced);
     bytes_iter.next();
     while let Some(b) = bytes_iter.next() {
         advanced += 1;
@@ -45,15 +55,17 @@ fn decode_integer(bytes: &[u8]) -> (Vec<u8>, usize) {
             break;
         }
         int.push(*b);
+        // println!("advanced={} int={:?}", advanced, int);
     }
-    (int, advanced)
+    (int, advanced + 1)
 }
 
 fn decode_string(bytes: &[u8], position: usize) -> (Vec<u8>, usize) {
-    let string_length = bytes_to_integer(&[bytes[position]]) as usize + 2;
-    let decoded = bytes[position + 2..position + string_length as usize].to_vec();
-    println!("decoded={:?}", decoded);
-    (decoded, string_length)
+    let string_length = bytes_to_integer(&[bytes[position]]) as usize;
+    let decoded = bytes[position + 2..position + 2 + string_length].to_vec();
+    // println!("decoded={:?}", decoded);
+    // println!("string length={}", string_length);
+    (decoded, string_length + 2)
 }
 
 fn decode_list(bytes: &[u8], position: usize) -> (Vec<u8>, usize) {
@@ -68,8 +80,8 @@ fn bytes_to_integer(bytes: &[u8]) -> i64 {
     std::str::from_utf8(bytes).unwrap().parse::<i64>().unwrap()
 }
 
-fn bytes_to_string(bytes: &[u8]) -> &str {
-    std::str::from_utf8(bytes).unwrap()
+fn bytes_to_string(bytes: &[u8]) -> String {
+    std::str::from_utf8(bytes).unwrap().to_owned()
 }
 
 #[cfg(test)]
@@ -79,18 +91,18 @@ mod tests {
     #[test]
     fn test_decode_integer() {
         let int1 = "i12362e".as_bytes();
-        let decoded1 = decode(int1);
+        let (decoded1, _) = decode_integer(int1, 0);
         let decoded_int1 = bytes_to_integer(&decoded1);
         assert_eq!(decoded_int1, 12362);
 
         let int2 = "i-3e".as_bytes();
-        let decoded2 = decode(int2);
+        let (decoded2, _) = decode_integer(int2, 0);
         let decoded_int2 = bytes_to_integer(&decoded2);
         assert_eq!(decoded_int2, -3);
 
         // TODO: this should not be valid.
         let int3 = "i03e".as_bytes();
-        let decoded3 = decode(int3);
+        let (decoded3, _) = decode_integer(int3, 0);
         let decoded_int3 = bytes_to_integer(&decoded3);
         assert_eq!(decoded_int3, 3);
     }
@@ -98,18 +110,18 @@ mod tests {
     #[test]
     fn test_decode_string() {
         let string1 = "4:spam".as_bytes();
-        let decoded1 = decode(string1);
+        let (decoded1, _) = decode_string(string1, 0);
         let decoded_string1 = bytes_to_string(&decoded1);
-        assert_eq!(decoded_string1, "spam");
+        assert_eq!(decoded_string1, "spam".to_string());
 
         let string2 = "0:".as_bytes();
-        let decoded2 = decode(string2);
+        let (decoded2, _) = decode_string(string2, 0);
         let decoded_string2 = bytes_to_string(&decoded2);
-        assert_eq!(decoded_string2, "");
+        assert_eq!(decoded_string2, "".to_string());
 
         let string3 = "8:announce".as_bytes();
-        let decoded3 = decode(string3);
+        let (decoded3, _) = decode_string(string3, 0);
         let decoded_string3 = bytes_to_string(&decoded3);
-        assert_eq!(decoded_string3, "announce");
+        assert_eq!(decoded_string3, "announce".to_string());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,13 @@ fn main() {
     let string = "4:spam".as_bytes();
     let a = decode(string);
     println!("result={:?}", a);
+    let integer = "i123e".as_bytes();
+    let b = decode(integer);
+    println!("result={:?}", b);
+    let intstring = "i123e4:spam".as_bytes();
+    let c = decode(intstring);
+    println!("result={:?}", c);
+    let stringint = "4:spami123e".as_bytes();
+    let d = decode(stringint);
+    println!("result={:?}", d);
 }
